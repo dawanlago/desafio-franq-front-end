0d58523343b547d2f5180003c2ab9443976e9224
dawanlago <dawanlago1@gmail.com>
2023-01-17 15:43:21 -0300

    Criação das telas de login e nova conta

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="login" class="container">
+    <div class="area-login">
+      <v-icon color="#4D61FC" size="30">
+        mdi-login-variant
+      </v-icon>
+      <h1 class="title">Bem vindo(a)</h1>
+      <h2 class="subtitle">Faça login em sua conta</h2>
+      <v-form>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field label="Email" type="email"></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-text-field label="Senha" type="password"></v-text-field>
+          </v-col>
+          <v-btn block x-large color="primary">
+            Entrar
+          </v-btn>
+          <v-btn block x-large class="subtitle mt-4" @click='handleNewAccount'>
+            Criar conta
+          </v-btn>
+        </v-row>
+      </v-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    handleNewAccount() {
+      this.$router.push('new-account');
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.container {
+  height: calc(100vh);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.area-login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: var(--light) !important;
+  width: 500px;
+  padding: 40px;
+  border-radius: 5px;
+}
+</style>
---
+++ src/views/NewAccount.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="login" class="container">
+    <div class="area-criacao">
+      <v-icon color="#4D61FC" size="30">
+        mdi-account
+      </v-icon>
+      <h1 class="title">Criação de conta</h1>
+      <v-form v-model="valid">
+          <v-row>
+            <v-col cols="12">
+              <v-text-field label="Nome" type="text"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field label="Email" type="email"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field label="Senha" type="password"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field label="Repita a sua senha" type="password"></v-text-field>
+            </v-col>
+            <v-btn block x-large color="primary" class="subtitle">Criar conta</v-btn>
+          </v-row>
+      </v-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang='scss' scoped>
+.container {
+  height: calc(100vh);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.area-criacao {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: var(--light) !important;
+  width: 500px;
+  padding: 40px;
+  border-radius: 5px;
+}
+</style>
