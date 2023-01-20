3eb044e8094c518d44bd950e6dc1267a3e7a3a5a
dawanlago <dawanlago1@gmail.com>
2023-01-20 17:45:05 -0300

    Configuração heroku

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-const cors = require('cors');
-
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
 const app = express();
 
-app.use(cors(corsOptions));
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 app.get(/.*/, function (req, res) {
