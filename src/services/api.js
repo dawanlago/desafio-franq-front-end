1f98b5f37d90ac89deab938f7eeb527e8802aa6e
dawanlago <dawanlago1@gmail.com>
2023-01-20 17:14:47 -0300

    Configuração heroku

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,11 @@ const api = () => {
   const axiosInstance = axios.create({
     baseURL: `https://api.hgbrasil.com/finance?format=json-cors&key=${KEY}`,
     withCredentials: false,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+    },
+
   });
 
   return axiosInstance;
